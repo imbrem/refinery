4f67a99858fe3689d7f23b34a2d4d8c1e3688c3b
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-02-25 21:04:23 +0000

    Semantic weakening

--- Refinery/Term/Extrinsic/Semantics/Wk.lean
+++ Refinery/Term/Extrinsic/Semantics/Wk.lean
@@ -19,52 +19,60 @@ variable {φ : Type _} {α : outParam (Type _)} {ε : outParam (Type _)} [S : Si
 
 namespace Term
 
--- theorem Deriv.wkD_den {Γ Δ : Ctx? α} (ρ : Γ.Wk Δ) {A : Ty α} {a : Term φ (Ty α)} (D : Δ ⊢ a : A)
---   : (D.wkD ρ).den (C := C) = ρ.den ≫ D.den
---   := by induction D generalizing Γ with
---   | let₁ hΓ da db Ia Ib =>
---     stop
---     simp only [
---       wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc,
---       <-PremonoidalCategory.whiskerLeft_comp_assoc, <-Ia
---     ]
---     rw [Central.left_exchange_assoc, Ib]
---     simp
---   | pair hΓ da db Ia Ib =>
---     stop
---     simp only [
---       wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc, ltimes,
---       Central.left_exchange_assoc, Central.left_exchange,
---     ]
---     rw [Ia, Ib]
---     simp [Central.right_exchange_assoc]
---   | let₂ hΓ da db Ia Ib =>
---     stop
---     simp only [
---       wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc,
---       <-PremonoidalCategory.whiskerLeft_comp_assoc, <-Ia
---     ]
---     rw [Central.left_exchange_assoc, Ib]
---     simp
---   | case hΓ da db dc Ia Ib Ic =>
---     stop
---     simp only [wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc, Ty.den]
---     congr 1
---     rw [
---       Central.left_exchange_assoc, Central.left_exchange_assoc,
---       DistributiveCategory.distl_inv_naturality_left_assoc, Ib, Ic,
---       <-PremonoidalCategory.whiskerLeft_comp_assoc, Ia
---     ]
---     simp
---   | iter hΓ hc hd da db Ia Ib =>
---     simp only [
---       wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc, Ia,
---       PremonoidalCategory.whiskerLeft_comp_assoc
---     ]
---     rw [Central.left_exchange_assoc, Central.left_exchange_assoc]
---     congr
---     -- TODO: by uniformity
---     apply Eq.symm
---     apply E.pure_uniform
---     sorry
---   | _ => stop simp [wkD, den, *]
+theorem Deriv.wkD_den {Γ Δ : Ctx? α} (ρ : Γ.Wk Δ) {A : Ty α} {a : Term φ (Ty α)} (D : Δ ⊢ a : A)
+  : (D.wkD ρ).den (C := C) = ρ.den ≫ D.den
+  := by induction D generalizing Γ with
+  | let₁ hΓ da db Ia Ib =>
+    simp only [
+      wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc,
+      <-PremonoidalCategory.whiskerLeft_comp_assoc, <-Ia
+    ]
+    rw [Central.left_exchange_assoc, Ib]
+    simp
+  | pair hΓ da db Ia Ib =>
+    simp only [
+      wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc, ltimes,
+      Central.left_exchange_assoc, Central.left_exchange,
+    ]
+    rw [Ia, Ib]
+    simp [Central.right_exchange_assoc]
+  | let₂ hΓ da db Ia Ib =>
+    simp only [
+      wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc,
+      <-PremonoidalCategory.whiskerLeft_comp_assoc, <-Ia
+    ]
+    rw [Central.left_exchange_assoc, Ib]
+    simp
+  | case hΓ da db dc Ia Ib Ic =>
+    simp only [wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc, Ty.den]
+    congr 1
+    rw [
+      Central.left_exchange_assoc, Central.left_exchange_assoc,
+      DistributiveCategory.distl_inv_naturality_left_assoc, Ib, Ic,
+      <-PremonoidalCategory.whiskerLeft_comp_assoc, Ia
+    ]
+    simp
+  | iter hΓ hc hd da db Ia Ib =>
+    simp only [
+      wkD, den, Ctx?.SSplit.wk_den_assoc, tensorHom_def, Category.assoc, Ia,
+      PremonoidalCategory.whiskerLeft_comp_assoc
+    ]
+    rw [Central.left_exchange_assoc, Central.left_exchange_assoc]
+    congr
+    apply Eq.symm
+    apply E.pure_uniform
+    rw [<-PremonoidalCategory.comp_whiskerRight_assoc, Model.copy_rel_ltimes ⊥, Ib]
+    simp only [ltimes, comp_whiskerRight, whisker_assoc, Ty.den.eq_5, Category.assoc,
+      Iso.inv_hom_id_assoc, Ctx?.Wk.den, ge_iff_le, le_top, Var?.Wk.den_used, eqToHom_refl,
+      tensorHom_id, PremonoidalCategory.whiskerLeft_comp, desc_comp, inl_desc, Category.id_comp,
+      inr_desc]
+    rw [
+      PremonoidalCategory.associator_naturality_left_assoc,
+      Central.left_exchange_assoc, Central.left_exchange_assoc,
+      DistributiveCategory.distl_inv_naturality_left_assoc
+    ]
+    simp only [
+      desc_comp, Category.assoc, inl_desc, inr_desc, Category.id_comp,
+      <-PremonoidalCategory.comp_whiskerRight_assoc, Model.drop_aff ⊥
+    ]
+  | _ => simp [wkD, den, *]
