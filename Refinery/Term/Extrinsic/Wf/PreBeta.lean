38cfafa0d5d43d5c45c74d33b38c9ad8b6cce52b
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-03-28 00:58:48 +0000

    Further prebeta lore

--- Refinery/Term/Extrinsic/Wf/PreBeta.lean
+++ Refinery/Term/Extrinsic/Wf/PreBeta.lean
@@ -40,3 +40,27 @@ theorem Wf.pre_beta {Γ Γl Γr : Ctx? α} {A : Ty α}
     : a.let₁ hΓ (b.pwk ((Ctx?.PWk.refl Γl).cons (by simp)))
     ≈ b.subst (SubstDS.subst0 hΓ a.deriv q hq)
     := ⟨pre_beta_pos hΓ a q hq b he.left heq.left, pre_beta_neg hΓ a q hq b he.right heq.right⟩
+
+theorem Wf.pre_beta_pureIn {Γ Γl Γr : Ctx? α} {A : Ty α}
+    (hΓ : Γ.SSplit Γl Γr) (a : Wf R Γr A) (q : Quant) (hq : q ≤ quant Γr)
+    (b : Wf R (Γl.cons ⟨A, q⟩) B)
+    [ha : HasEff ⊥ a.tm]
+    : a.let₁ hΓ (b.pwk ((Ctx?.PWk.refl Γl).cons (by simp)))
+    ≈ b.subst (SubstDS.subst0 hΓ a.deriv q hq)
+    := pre_beta hΓ a q hq b (commutes_bot_left (r := ⊤)) (by simp [OrderedPQuant.pquant_bot])
+
+theorem Wf.pre_beta_pureLin {Γ Γl Γr : Ctx? α} {A : Ty α}
+    (hΓ : Γ.SSplit Γl Γr) (a : Wf R Γr A)
+    (b : Wf R (Γl.cons ⟨A, 1⟩) B)
+    [ha : HasEff ⊥ a.tm]
+    : a.let₁ hΓ (b.pwk ((Ctx?.PWk.refl Γl).cons (by simp)))
+    ≈ b.subst (SubstDS.subst0 hΓ a.deriv 1 bot_le)
+    := pre_beta_pureIn hΓ a 1 bot_le b
+
+theorem Wf.pre_beta_pureLout {Γ Γl Γr : Ctx? α} {A : Ty α}
+    (hΓ : Γ.SSplit Γl Γr) (a : Wf R Γr A)
+    (b : Wf R (Γl.cons ⟨A, 1⟩) B)
+    [hb : HasEff ⊥ b.tm]
+    : a.let₁ hΓ (b.pwk ((Ctx?.PWk.refl Γl).cons (by simp)))
+    ≈ b.subst (SubstDS.subst0 hΓ a.deriv 1 bot_le)
+    := pre_beta hΓ a 1 bot_le b (commutes_bot_right (l := ⊤)) bot_le
